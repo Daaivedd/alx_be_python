ee07bf381b8507efa5535406fe93e0922d19ee15
David Hanna <davidsamy955@gmail.com>
2024-06-23 22:58:49 +0300

    amendment the name of the variavbles

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
-income = int(input("Enter your monthly income: "))
-expenses = int(input("Enter your total monthly expenses: "))
-Monthly_Savings = income - expenses  
+monthly_income = int(input("Enter your monthly income: "))
+monthly_expenses = int(input("Enter your total monthly expenses: "))
+Monthly_Savings = monthly_income - monthly_expenses  
 print("your momnthly savings are {} " .format(Monthly_Savings))
 
 Projected_Savings = Monthly_Savings * 12 + (Monthly_Savings * 12 * 0.05)
