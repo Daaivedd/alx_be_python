20038c22d6637e5e5aee8100341eebf10c328ae4
David Hanna <davidsamy955@gmail.com>
2024-06-23 18:51:47 +0300

    Basic Arithmetic Exercise solution

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10 
+number2 = 5 
+sum = number1 + number2 
+sub = number1 - number2
+mul = number1 * number2
+
+print("Addition of {} and {} is {} " .format(number1,number2,sum) )
+print("subtraction of {} and {} is {}" .format(number1,number2,sub)) 
+print("multipication of {} and {} is {}" .format(number1,number2,mul))
