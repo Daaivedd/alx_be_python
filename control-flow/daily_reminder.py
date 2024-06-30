b2b1c070d6e4a326fc451dee493452f8a53f290e
David Hanna <davidsamy955@gmail.com>
2024-06-30 23:38:42 +0300

    modify brackets

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -3,15 +3,15 @@ priority = input("Priority (high/medium/low): ")
 time_bound = input("Is it time-bound? (yes/no): ")
 
 match priority :
-    case ("high") : 
+    case "high" : 
         if time_bound == "yes" :
             print(f"Reminder: {'task'} is a high priority task that requires immediate attention today!")
         else:
             msg =  "is a high priority task."
             print(f"Reminder: {'task'} {msg}")
-    case ("medium") :
+    case "medium" :
         msg = "is a low priority task. don't forget it."
         print(f"Note: {'task'} {msg}")
-    case ("low") :
+    case "low" :
         msg = "is a low priority task. Consider completing it when you have free time."
         print(f"Note: {'task'} {msg}")
