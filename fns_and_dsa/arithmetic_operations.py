55e05f0bf8da7a7191c43b5207ef09b40871a5f4
David Hanna <davidsamy955@gmail.com>
2024-07-14 21:53:32 +0300

    amendment spaces for second time

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,12 +1,12 @@
-def perform_operation(num1, num2, operation) :
+def perform_operation(num1, num2, operation):
     
-    if operation == 'add' :
+    if operation == 'add':
         return num1 + num2
-    elif operation == 'subtract' :
+    elif operation == 'subtract':
         return num1 - num2
-    elif operation == 'multiply' :
+    elif operation == 'multiply':
         return num1 * num2 
-    elif operation == 'divide' :
+    elif operation == 'divide':
         if num2 == 0 :
             return "Error: Division by zero"
         else :
