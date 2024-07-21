83cf14575a952c5013db6f4c8624ac04745bc4c0
David Hanna <davidsamy955@gmail.com>
2024-07-21 23:37:01 +0300

    change in spaces

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -4,8 +4,8 @@ class Shape:
     def area (Self):
         raise NotImplementedError ('you need to override this method!')
     
-class Rectangle (Shape):
-    def __init__(self, length, width) :
+class Rectangle(Shape):
+    def __init__(self, length, width):
         self.length = length
         self.width = width
 
@@ -13,8 +13,8 @@ class Rectangle (Shape):
         return self.length * self.width 
         
     
-class Circle (Shape):
-    def __init__(self, radius) :
+class Circle(Shape):
+    def __init__(self, radius):
         self.radius = radius
 
     def area (self):
