e7a614d1c1faad00f428fac19273840c703f4dd4
David Hanna <davidsamy955@gmail.com>
2024-07-21 23:20:02 +0300

    change in output formula

--- oop/library_system.py
+++ oop/library_system.py
@@ -4,7 +4,7 @@ class Book:
             self.author = author
 
         def __str__(self):
-            return f'{self.title} , {self.author}'
+            return f'Book: {self.title} , {self.author}'
 
 class EBook(Book):
       def __init__(self, title, author, file_size: int):
@@ -12,7 +12,7 @@ class EBook(Book):
             self.file_size = file_size
 
       def __str__(self):
-            return f'{self.title} , {self.author}, {self.file_size}'
+            return f'EBook: {self.title} , {self.author}, {self.file_size}'
 
 class PrintBook(Book):
       def __init__(self, title, author, page_count: int):
@@ -20,7 +20,7 @@ class PrintBook(Book):
             self.page_count = page_count
 
       def __str__(self):
-            return f'{self.title} , {self.author}, {self.page_count}'
+            return f'PrintBook: {self.title} , {self.author}, {self.page_count}'
 
 class Library():
       def __init__(self):
